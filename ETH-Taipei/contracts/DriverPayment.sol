bf8ed08a9943d0a4036a530964201a44e47b0ada
Jason <df41022@gmail.com>
2023-04-22 21:08:30 +0800

    update contract

--- ETH-Taipei/contracts/DriverPayment.sol
+++ ETH-Taipei/contracts/DriverPayment.sol
@@ -6,42 +6,32 @@ import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract DriverPayment {
+contract DriverPayment is Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
-    address _owner;
+    address owner;
     address private immutable uniswapRouter;
     address private immutable usdcToken;
-    uint256 public _REWARD_RATE;
-    uint256 public _STATION_SERVICE_FEE_RATE;
-    uint256 public _INTEREST_RATE;
-    uint256 public _PAY_INTEREST_PERIOD;
-    uint256 User_count;
-    uint256 Station_count;
-    mapping(address => uint256) Station_balances; //machi, not usdc
-    mapping(address => uint256) User_balances;
-    mapping(bytes32 => address) licenseToAddress;
-    mapping(address => uint256) Timestamp;
+    uint256 public rewardRate;
+    uint256 public stationServiceFeeRate;
+    uint256 public interestRate;
+    uint256 public payInterestPeriod;
+    mapping(address => uint256) private stationBalances; //machi, not usdc
+    mapping(address => uint256) private driverBalances;
+    mapping(bytes32 => address) private plateToAddress;
+    mapping(address => uint256) private timestamp;
 
     constructor(address _uniswapRouter, address _usdcToken) {
-        _owner = msg.sender;
+        owner = msg.sender;
         uniswapRouter = _uniswapRouter;
         usdcToken = _usdcToken;
-        User_count = 0;
-        Station_count = 0;
-        _REWARD_RATE = 0.05 ether; //5%
-        _INTEREST_RATE = 0.1 ether; //10%
-        _STATION_SERVICE_FEE_RATE = 0.02 ether; //2%
-        _PAY_INTEREST_PERIOD = 2592000; //month
+        rewardRate = 5 / 100; //5%
+        interestRate = 10 / 100; //10%
+        stationServiceFeeRate = 2 / 100; //2%
+        payInterestPeriod = 30 * 24 * 60 * 60; //month
     }
 
-    //if function with this modifier should satisfy owner check
-    modifier onlyOwner() {
-        require(msg.sender == _owner, "You are not owner");
-        _;
-    }
-
-    function depositWETH(uint256 amount) public payable {
+    function depositWETH(uint256 amount) public payable onlyOwner {
         //amount進來前就要*10^18
         address[] memory path = new address[](2);
         path[0] = IUniswapV2Router02(uniswapRouter).WETH();
@@ -49,6 +39,7 @@ contract DriverPayment {
         uint256 deadline = block.timestamp + 300; // 5 minute deadline
 
         // Call the Uniswap router's swapExactETHForTokens function to convert ETH to token
+        // jason: not sure how this functions work?
         uint256[] memory amounts = IUniswapV2Router02(uniswapRouter)
             .swapExactETHForTokens{value: msg.value}(
             amount,
@@ -56,67 +47,76 @@ contract DriverPayment {
             address(this),
             deadline
         );
-        User_balances[msg.sender] += amounts[1];
+        // jason: what are the items inside of amounts?
+        driverBalances[msg.sender] += amounts[1];
+    }
+
+    function payInterest(address _driver, uint times) internal onlyOwner {
+        for (uint i = times; i > 0; i--) {
+            driverBalances[_driver] *= interestRate;
+        }
+        timestamp[_driver] = block.timestamp;
+    }
+
+    function updateDriverBalance(address _driver) internal onlyOwner {
+        uint times = (timestamp[_driver] - block.timestamp) / payInterestPeriod;
+        if (times > 0) {
+            payInterest(_driver, times);
+        }
+    }
+
+    function getDriverBalance(address _driver) public onlyOwner {
+        updateDriverBalance(_driver);
+        return driverBalances[_driver];
     }
 
     function pay(
-        string memory license,
-        address gasStation,
-        uint256 amount
+        string memory _plate,
+        address _gasStation,
+        uint256 _amount
     ) public onlyOwner {
-        bytes32 license_Hash = keccak256(abi.encodePacked(license));
-        address driver_address = licenseToAddress[license_Hash];
-        require(User_balances[driver_address] >= amount);
-        User_balances[driver_address] -= amount;
-        User_balances[gasStation] += amount;
-        uint256 _reward = (amount * _REWARD_RATE) / 1e18;
-        User_balances[driver_address] += _reward;
+        bytes32 plateHash = keccak256(abi.encodePacked(_plate));
+        address driverAddress = plateToAddress[plateHash];
+        require(driverBalances[driverAddress] >= _amount);
+        driverBalances[driverAddress] -= _amount;
+        driverBalances[_gasStation] += _amount;
+        uint256 _reward = _amount * rewardRate;
+        driverBalances[driverAddress] += _reward;
     }
 
-    function pay_interest(
-        address driver_address,
-        uint times
-    ) internal onlyOwner {
-        for (uint i = times; i > 0; i--) {
-            User_balances[driver_address] *= _INTEREST_RATE / 1e18;
-        }
-        Timestamp[driver_address] =
-            block.timestamp -
-            (block.timestamp % _PAY_INTEREST_PERIOD);
-    }
-
-    function driver_watch_balance(
-        address driver_address
-    ) public returns (uint256) {
-        uint times = Timestamp[driver_address] -
-            block.timestamp /
-            _PAY_INTEREST_PERIOD;
-        if (times > 0) {
-            pay_interest(driver_address, times);
-        }
-        return User_balances[driver_address];
-    }
-
-    function registerCar(
-        string memory license,
-        address driver_address
+    function createRegistry(
+        string memory plate,
+        address driverAddress
     ) public onlyOwner {
-        bytes32 licenseHash = keccak256(abi.encodePacked(license));
-        licenseToAddress[licenseHash] = driver_address;
+        bytes32 plateHash = keccak256(abi.encodePacked(plate));
+        plateToAddress[plateHash] = driverAddress;
     }
 
-    function user_balanceOf() external view returns (uint256) {
-        return User_balances[msg.sender];
+    // TODO
+    // function readRegistry()  returns () {
+
+    // }
+
+    // function updateRegistry()  returns () {
+
+    // }
+
+    // function deleteRegistry() returns () {
+
+    // }
+
+    function driverBalanceOf() external view returns (uint256) {
+        return driverBalances[msg.sender];
     }
 
-    function Station_balanceOf() external view returns (uint256) {
-        return Station_balances[msg.sender];
+    function stationBalanceOf() external view returns (uint256) {
+        return stationBalances[msg.sender];
     }
 
-    function Station_withdrawUsdc(uint256 amount) external {
-        require(Station_balances[msg.sender] >= amount);
-        Station_balances[msg.sender] -= amount;
-        amount *= _STATION_SERVICE_FEE_RATE / 1e18;
+    function stationWithdrawUsdc(uint256 amount) external {
+        require(stationBalances[msg.sender] >= amount);
+        stationBalances[msg.sender] -= amount;
+        amount *= stationServiceFeeRate;
         IERC20(usdcToken).safeTransfer(msg.sender, amount);
     }
 }
