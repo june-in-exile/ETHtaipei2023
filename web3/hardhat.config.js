1320520827ca1624e4a989b586d39f4c25518c6f
Jason <df41022@gmail.com>
2023-04-23 10:08:27 +0800

    update

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -61,15 +61,6 @@ module.exports = {
   etherscan: {
     customChains: [
       {
-        network: "chiado",
-        chainId: 10200,
-        urls: {
-          //Blockscout
-          apiURL: "https://blockscout.com/gnosis/chiado/api",
-          browserURL: "https://blockscout.com/gnosis/chiado",
-        },
-      },
-      {
         network: "gnosis",
         chainId: 100,
         urls: {
@@ -86,7 +77,6 @@ module.exports = {
     apiKey: {
       //4) Insert your Gnosisscan API key
       //blockscout explorer verification does not require keys
-      chiado: "your key",
       gnosis: "your key",
     },
   }
