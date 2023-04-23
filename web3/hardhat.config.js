1887e934a29adac3bc77670963a69573b761064d
Jason <df41022@gmail.com>
2023-04-23 11:24:28 +0800

    gnosis

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,44 +1,12 @@
-// require('dotenv').config();
-// require("@nomiclabs/hardhat-ethers");
-// require("@nomiclabs/hardhat-waffle");
-// require("@nomiclabs/hardhat-etherscan");
-
-// const { API_URL, GOERLI_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
-
-// /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.18",
-//   defaultNetwork: "gnosis",
-//   networks: {
-//     hardhat: {},
-//     goerli: {
-//       url: API_URL,
-//       accounts: [`0x${GOERLI_PRIVATE_KEY}`]
-//     },
-//     gnosis: {
-//       url: "https://rpc.gnosischain.com",
-//       accounts: accounts,
-//     },
-//     chiado: {
-//       url: "https://rpc.chiadochain.net",
-//       gasPrice: 1000000000,
-//       accounts: accounts,
-//     },
-//   },
-//   etherscan: {
-//     apiKey: ETHERSCAN_API_KEY,
-//   }
-// };
-
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require("@nomicfoundation/hardhat-toolbox");
 
-//https://hardhat.org/hardhat-runner/docs/config#json-rpc-based-networks
+const { PRIVATE_KEY } = process.env;
 
-//Note: keep your mnemonic and private keys securely
-//Read more: https://hardhat.org/hardhat-runner/docs/config#hd-wallet-config
 //1) You can configure private keys or mnemonic:
 //let accounts = ["your private key here"]
-let accounts = { mnemonic: "your mnemonic here", }
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -50,16 +18,25 @@ module.exports = {
     },
     gnosis: {
       url: "https://rpc.gnosischain.com",
-      accounts: accounts,
+      accounts: [PRIVATE_KEY],
     },
     chiado: {
       url: "https://rpc.chiadochain.net",
       gasPrice: 1000000000,
-      accounts: accounts,
+      accounts: [PRIVATE_KEY],
     },
   },
   etherscan: {
     customChains: [
+      {
+        network: "chiado",
+        chainId: 10200,
+        urls: {
+          //Blockscout
+          apiURL: "https://blockscout.com/gnosis/chiado/api",
+          browserURL: "https://blockscout.com/gnosis/chiado",
+        },
+      },
       {
         network: "gnosis",
         chainId: 100,
@@ -77,6 +54,7 @@ module.exports = {
     apiKey: {
       //4) Insert your Gnosisscan API key
       //blockscout explorer verification does not require keys
+      chiado: "your key",
       gnosis: "your key",
     },
   }
--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -2,7 +2,7 @@ const { network, ethers } = require("hardhat");
 
 async function main() {
   const DriverPayment = await ethers.getContractFactory("DriverPayment");
-  const driverPayment = await DriverPayment.deploy('0x4648a43b2c14da09fdf82b161150d3f634f40491', '0xd87ba7a50b2e7e660f678a895e4b72e7cb4ccd9c', '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6');
+  const driverPayment = await DriverPayment.deploy('0x9637b74190bf2FAb40f0EC50f0a7944Fc00E93b6');
 
   console.log('Deploying DriverPayment...');
   await driverPayment.deployed();
