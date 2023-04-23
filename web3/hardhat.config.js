92079821e6efcba76e6466ae5f1a9c93cb42d631
Jason <df41022@gmail.com>
2023-04-23 08:54:49 +0800

    update sol

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -3,22 +3,19 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 
-const {API_URL, GOERLI_PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
+const { API_URL, GOERLI_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  defaultNetwork: "ganache",
   networks: {
-    ganache:{
-      url:'http://127.0.0.1:8545',
-    },
-    goerli:{
+    hardhat: {},
+    goerli: {
       url: API_URL,
-      accounts:[`0x${GOERLI_PRIVATE_KEY}`]
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`]
     }
   },
-  etherscan:{
-    apiKey:ETHERSCAN_API_KEY,
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   }
 };
